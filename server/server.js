998950095e42b97be96b7a9b571aa1c9f062c071
Al Ferreyra <al@iMac-de-Al.local>
2020-07-08 02:36:04 -0500

    Tercer commit dist corregido

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 var app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-var app_path = '../dist/crecentip';
+var app_path = '../dist/app-capem';
 app.use('/', express.static(path.join(__dirname, app_path)))
 .listen(PORT, () => console.log(`Escuchando el puerto ${PORT}`));
 
